e60693011cb85d9b3431958eea8acfa9c8790f34
ihihifatima <ihihifatima@outlook.com>
2021-11-14 19:43:23 +0100

    Ajout de PrimeFace

--- src/main/java/ma/emsi/tpcustomerapplication1/session/CustomerManager.java
+++ src/main/java/ma/emsi/tpcustomerapplication1/session/CustomerManager.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
+import javax.ejb.LocalBean;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -22,10 +23,10 @@ import ma.emsi.tpcustomerapplication1.Customer;
 @Stateless
 public class CustomerManager {
 
-    @PersistenceContext(unitName = "customerPU")
+    @PersistenceContext(name = "customerPU")
     private EntityManager em;
-    @Resource
-    private javax.transaction.UserTransaction utx;
+   // @Resource
+  //  private javax.transaction.UserTransaction utx;
 
     public List<Customer> getAllCustomers() { 
       Query query =em.createNamedQuery("Customer.findAll");
@@ -37,14 +38,15 @@ public class CustomerManager {
     }     
 
     public void persist(Customer customer) {
-        try {
-            utx.begin();
+          em.persist(customer);
+        /*try {
+            //utx.begin();
             em.persist(customer);
-            utx.commit();
+           // utx.commit();
         } catch (Exception e) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
             throw new RuntimeException(e);
-        }
+        }*/
     }
     
     
